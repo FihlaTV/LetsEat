fc9ee77c71156bc58c832a60c70935eae4c7e881
Alban Lautie <mail@albanlautie.fr>
2016-11-16 22:24:23 +0100

    [Evo][FO] LE-15 - Finish search page

---
+++ frontoffice/www/js/controllers/SearchController.js
@@ -0,0 +1,41 @@
+app.controller('searchCtrl', function($scope, $DateService) {
+
+    $scope.credential = {
+        'city': '',
+        'date': '',
+        'participant': 1
+    }
+    $scope.step = 'city'
+    $scope.dates = []
+
+    $scope.submit = function() {
+        if ($scope.step == 'city')
+            $scope.step = 'date'
+        else
+            $scope.step = 'recap'
+    }
+
+    $scope.search = function(credential) {
+        console.log(credential)
+    }
+
+    $scope.changeStep = function (step) {
+        $scope.step = step
+    }
+
+    $scope.incrementParticipant = function() {
+        $scope.credential.participant++
+    }
+
+    $scope.decrementParticipant = function() {
+        if($scope.credential.participant > 1)
+            $scope.credential.participant--
+    }
+
+    var dateBase = new Date()
+    setTimeout(function() {
+        for(var i = 0; i < 12; i++)
+            $scope.dates.push($DateService.addMonth(dateBase))
+    }, 10)
+
+})
