19d92b9334c7e65583a25817d2d07c618c0fd4e7
Alban Lautie <mail@albanlautie.fr>
2016-11-18 09:35:15 +0100

    add img

--- frontoffice/www/js/controllers/BookingController.js
+++ frontoffice/www/js/controllers/BookingController.js
@@ -1,5 +1,15 @@
-app.controller('bookingCtrl', function($scope, $http) {
+app.controller('bookingCtrl', function($scope, $http, $LocalStorageService) {
 
-    //$http.get("")
+    const user = $LocalStorageService.getObject("user_profile")
+    $http.get("http://5.196.67.70:5000/letseat-api/events/booking/"+user.id_user).then(function(res) {
+        $scope.bookings = res.data
+    })
+
+    $scope.formatDate = function(date) {
+        if(date !== undefined) {
+            var dateOut = new Date(date.toString().substring(0, 19))
+            return dateOut
+        }
+    }
 
 })
